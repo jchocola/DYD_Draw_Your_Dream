ca7e2635d86ddc64d929b978d908118c9d4b8fd5
Bak Nguen <sangsangden@gmail.com>
2026-01-30 07:37:16 +0700

    firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:dyd_drawer/core/router/router.dart';
+import 'package:dyd_drawer/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/web.dart';
 
 // debug logger
 final logger = Logger();
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+
   runApp(const MyApp());
 }
 
@@ -17,6 +25,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: router,
-      title: 'DYD - Draw Your Dream');
+      title: 'DYD - Draw Your Dream',
+    );
   }
 }
