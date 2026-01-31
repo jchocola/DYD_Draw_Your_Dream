b34b752aef11629c050dc6dd27bdd3c5d04eaed0
Bak Nguen <sangsangden@gmail.com>
2026-01-31 19:32:14 +0700

    add plugin + color picker pallete

--- lib/feature/feature_painter/presentation/new_painter_page.dart
+++ lib/feature/feature_painter/presentation/new_painter_page.dart
@@ -3,11 +3,22 @@ import 'package:dyd_drawer/core/constant/app_constant.dart';
 import 'package:dyd_drawer/core/icon/app_icon.dart';
 import 'package:dyd_drawer/feature/feature_painter/widget/editing_board.dart';
 import 'package:dyd_drawer/feature/feature_painter/widget/menu_tool_bar.dart';
+import 'package:dyd_drawer/main.dart';
 import 'package:dyd_drawer/shared/custom_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_painter/simple_painter.dart';
 
-class NewPainterPage extends StatelessWidget {
+class NewPainterPage extends StatefulWidget {
   const NewPainterPage({super.key});
+
+  @override
+  State<NewPainterPage> createState() => _NewPainterPageState();
+}
+
+class _NewPainterPageState extends State<NewPainterPage> {
+  final PainterController controller = PainterController(
+    settings: PainterSettings(),
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -18,10 +29,7 @@ class NewPainterPage extends StatelessWidget {
         withAction: true,
         action: IconButton(onPressed: () {}, icon: Icon(AppIcon.checkIcon)),
       ),
-      body: Background(
-        path: AppConstant.appBg,
-        child: _buildBody(context),
-      ),
+      body: Background(path: AppConstant.appBg, child: _buildBody(context)),
     );
   }
 
@@ -31,9 +39,84 @@ class NewPainterPage extends StatelessWidget {
         padding: EdgeInsets.symmetric(horizontal: AppConstant.appPadding),
         child: Column(
           spacing: AppConstant.appPadding,
-          children: [MenuToolBar(), EditingBoard()],
+          children: [
+            MenuToolBar(),
+
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    logger.d('Toggle drawing');
+                    controller.toggleDrawing();
+                  },
+                  icon: Icon(Icons.brush),
+                ),
+                IconButton(
+                  onPressed: () {
+                    logger.d('Toggle drawing');
+                    controller.changeBrushValues(size: 10, color: Colors.red);
+                  },
+                  icon: Icon(Icons.brush),
+                ),
+
+                 IconButton(
+                  onPressed: () {
+                    logger.d('Toggle drawing');
+                    controller.changeBrushValues(size: 10, color: Colors.cyanAccent);
+                  },
+                  icon: Icon(Icons.brush),
+                ),
+
+                IconButton(
+                  onPressed: () {
+                    logger.d('Toggle drawing');
+                    controller.toggleErasing();
+                    controller.changeEraseValues(size: 10, color: Colors.black87);
+                  },
+                  icon: Icon(Icons.brush),
+                ),
+
+                IconButton(
+                  onPressed: () async{
+                    logger.d('Toggle drawing');
+                final image =    await controller.renderImage();
+
+                logger.d('Rendered image: $image');
+                  },
+                  icon: Icon(Icons.brush),
+                ),
+
+                 IconButton(
+                  onPressed: () async{
+                    logger.d('Toggle drawing');
+                 controller.addShape(ShapeType.circle);
+
+               
+                  },
+                  icon: Icon(Icons.brush),
+                ),
+              ],
+            ),
+
+            SizedBox(
+              height: 400,
+              width: double.infinity,
+              child: ClipRRect(
+                borderRadius: BorderRadiusGeometry.circular(AppConstant.borderRadius),
+                child: PainterWidget(
+                 // boundaryMargin: 0.0,
+                  controller: controller),
+              )),
+            //EditingBoard()
+          ],
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
--- lib/feature/feature_painter/widget/menu_tool_bar.dart
+++ lib/feature/feature_painter/widget/menu_tool_bar.dart
@@ -1,5 +1,8 @@
+import 'package:dyd_drawer/core/constant/app_constant.dart';
 import 'package:dyd_drawer/core/icon/app_icon.dart';
+import 'package:dyd_drawer/shared/color_pallete_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_popup/flutter_popup.dart';
 
 class MenuToolBar extends StatelessWidget {
   const MenuToolBar({super.key});
@@ -7,15 +10,55 @@ class MenuToolBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final iconColor = Theme.of(context).colorScheme.tertiary;
-    final iconButtonColor = Theme.of(context).colorScheme.tertiary.withOpacity(0.2);
+    final iconButtonColor = Theme.of(
+      context,
+    ).colorScheme.tertiary.withOpacity(0.2);
+    final popupKey = GlobalKey<CustomPopupState>();
+    final theme = Theme.of(context);
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
-        IconButton.filled(onPressed: () {}, icon: Icon(AppIcon.downloadIcon, color: iconColor,), style: IconButton.styleFrom(backgroundColor: iconButtonColor)),
-        IconButton.filled(onPressed: () {}, icon: Icon(AppIcon.imageIcon , color: iconColor,), style: IconButton.styleFrom(backgroundColor: iconButtonColor)),
-        IconButton.filled(onPressed: () {}, icon: Icon(AppIcon.pencilIcon, color: iconColor,), style: IconButton.styleFrom(backgroundColor: iconButtonColor)),
-        IconButton.filled(onPressed: () {}, icon: Icon(AppIcon.ereaserIcon, color: iconColor,), style: IconButton.styleFrom(backgroundColor: iconButtonColor)),
-        IconButton.filled(onPressed: () {}, icon: Icon(AppIcon.colorLensIcon, color: iconColor,), style: IconButton.styleFrom(backgroundColor: iconButtonColor)),
+        IconButton.filled(
+          onPressed: () {},
+          icon: Icon(AppIcon.downloadIcon, color: iconColor),
+          style: IconButton.styleFrom(backgroundColor: iconButtonColor),
+        ),
+        IconButton.filled(
+          onPressed: () {},
+          icon: Icon(AppIcon.imageIcon, color: iconColor),
+          style: IconButton.styleFrom(backgroundColor: iconButtonColor),
+        ),
+        IconButton.filled(
+          onPressed: () {},
+          icon: Icon(AppIcon.pencilIcon, color: iconColor),
+          style: IconButton.styleFrom(backgroundColor: iconButtonColor),
+        ),
+        IconButton.filled(
+          onPressed: () {},
+          icon: Icon(AppIcon.ereaserIcon, color: iconColor),
+          style: IconButton.styleFrom(backgroundColor: iconButtonColor),
+        ),
+
+
+
+        ///
+        /// COLOR PICKER
+        ///
+        CustomPopup(
+          //backgroundColor: theme.colorScheme.onPrimaryContainer,
+          contentPadding: EdgeInsets.symmetric(
+            horizontal: AppConstant.appPadding,
+          ),
+          key: popupKey,
+          content: ColorPalletePicker(),
+          child: IconButton.filled(
+            onPressed: () {
+              popupKey.currentState?.show();
+            },
+            icon: Icon(AppIcon.colorLensIcon, color: iconColor),
+            style: IconButton.styleFrom(backgroundColor: iconButtonColor),
+          ),
+        ),
       ],
     );
   }
